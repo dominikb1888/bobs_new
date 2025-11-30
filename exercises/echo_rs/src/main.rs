804e17126cd1cdd923f853fe0d64c21ec4ff8add
Dominik Boehler <dominik.boehler@gmx.net>
2025-11-30 16:04:57 +0100

    added echo_rs

---
+++ exercises/echo_rs/src/main.rs
@@ -0,0 +1,24 @@
+use clap::Parser; 
+
+#[derive(Debug, Parser)] 
+#[command(author, version, about)] 
+/// Rust version of `echo` 
+struct Args { 
+    /// Input text 
+    #[arg(required(true))] 
+    text: Vec<String>, 
+    /// Do not print newline 
+    #[arg(short('n'))] 
+    omit_newline: bool, 
+}
+
+fn main() {
+    let args = Args::parse();
+    let result = args.text.join(" ");
+
+    if args.omit_newline == true {
+        print!("{result}");
+    } else {
+        println!("{result}")
+    }
+}
